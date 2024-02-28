39d83b372a7d0dd3e97bcc15304ae6d256bffd22
Maxim Alpatski <MaximAlpatski@gmail.com>
2024-02-29 01:30:38 +0400

    Add first picture

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Picture from './components/Picture';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Picture from './components/main/Picture'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Picture uri='https://i.natgeofe.com/k/8fa25ea4-6409-47fb-b3cc-4af8e0dc9616/red-eyed-tree-frog-on-leaves-3-2_3x4.jpg' />
+      <Picture />
+      <Text>This is a frog</Text>
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
--- components/main/Picture.js
+++ components/main/Picture.js
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function Picture({ uri }) {
+export default function Picture() {
     return (
-        <Image style={styles.picture} source={require(uri)} />
+        <Image 
+        source={{
+            uri: 'https://i.natgeofe.com/k/8fa25ea4-6409-47fb-b3cc-4af8e0dc9616/red-eyed-tree-frog-on-leaves-3-2_3x4.jpg',
+        }}
+        style={styles.picture}
+      />
     )
 }
 
 const styles = StyleSheet.create({
     picture: {
-        width: '70%',
+        width: 300,
+        height: 300,
         borderRadius: 8,
+        borderColor: '#000000'
     }
 })
